7d2c01c1182b117f87eea3edcf5fcf3ec1c05794
Brett New and Sami Al-Jamal <brett+sami@>
2016-08-15 14:26:17 -0700

    allow user/admin to add/delete announcements.

--- app/routes/announcement.js
+++ app/routes/announcement.js
@@ -6,6 +6,11 @@ export default Ember.Route.extend({
   },
 
   actions:{
+    save3(params){
+      var newAnnouncement = this.store.createRecord('announcement', params);
+      newAnnouncement.save();
+      this.transitionTo('announcement');
+    },
     destroyAnnouncement(announcement){
       announcement.destroyRecord();
       this.transitionTo('announcement');
